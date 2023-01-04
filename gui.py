d29b36b848f40c67e9a1ae1124f2b0fd92f1883d
Akhil <bakhil696@gmail.com>
2023-01-04 21:36:11 +0530

    Create basic GUI (w/o interaction)

--- gui.py
+++ gui.py
@@ -1,5 +1,13 @@
 from functions import getTodos, writeTodos
 import time
-import PySimpleGUI
+import PySimpleGUI as sg
 
+label = sg.Text("Task to add:")
+inputBox = sg.InputText(tooltip = "Enter task")
 
+addButton = sg.Button("Add")
+
+#Create Instance
+window = sg.Window('ToDooo App', layout = [[label], [inputBox, addButton]])
+window.read()   #Displays the window
+window.close()  #Closes the window
