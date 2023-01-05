29cc827fc924f3ee981961f511c80df6b55b893f
Akhil <bakhil696@gmail.com>
2023-01-05 21:28:32 +0530

    Implement edit button

--- gui.py
+++ gui.py
@@ -4,12 +4,16 @@ import PySimpleGUI as sg
 
 label = sg.Text("Task to add:")
 inputBox = sg.InputText(tooltip = "Enter task", key='todo')
-
 addButton = sg.Button("Add")
+
+listbox = sg.Listbox(values=getTodos(), key='existingtodo',
+ enable_events=True, size=[45,10])
+
+editButton = sg.Button("Edit")
 
 #Create Instance
 window = sg.Window('ToDooo App',
-    layout = [[label], [inputBox, addButton]],
+    layout = [[label], [inputBox, addButton], [listbox, editButton]],
     font=('Helvetica', 13))
 
 while True:
@@ -21,7 +25,23 @@ while True:
             newtodo = values['todo'] + "\n"   #Stores the value of todo key in the dictionary
             todos.append(newtodo)
             writeTodos(todos)
+
+            window['existingtodo'].update(values=todos)
         
+        case "Edit":
+            todos = getTodos()
+            newtodo = values['todo'] + "\n"
+            selectedtodo = values['existingtodo'][0]
+
+            index = todos.index(selectedtodo)
+            todos[index] = newtodo
+            writeTodos(todos)
+
+            window['existingtodo'].update(values=todos)
+        
+        case "existingtodo":
+            window['todo'].update(value = values['existingtodo'][0])
+
         case sg.WIN_CLOSED:
             break
 
